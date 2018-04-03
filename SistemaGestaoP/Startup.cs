68a88f115244ce7a825cff16332242c909075b3b
kaze94phoenix <phoenix.kazekage@gmail.com>
2018-04-03 02:24:10 +0200

    Add project files.

---
+++ SistemaGestaoP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaGestaoP.Startup))]
+namespace SistemaGestaoP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
